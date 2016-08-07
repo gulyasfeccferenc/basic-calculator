be2bc7515f351b4e4056d6ac42a498edf5150373
Fecc <gulyasfeccferenc@gmail.com>
2016-08-07 21:33:55 +0200

    Make some noise \w yo angular generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name basicCalculatorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the basicCalculatorApp
+ */
+angular.module('basicCalculatorApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name basicCalculatorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the basicCalculatorApp
+ */
+angular.module('basicCalculatorApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
