478c2b931cbf08488cceb49b5d8abe6c2fe9ad10
Fecc <gulyasfeccferenc@gmail.com>
2016-08-07 22:15:18 +0200

    Still initializing

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('basicCalculatorApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    
   });
